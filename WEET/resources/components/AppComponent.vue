6df1aa48c36170239fc69ddea6e4614b4d249582
muhwa91 <muhwa91@gmail.com>
2024-02-27 16:39:18 +0900

    0227 css 테스트용

--- WEET/resources/components/AppComponent.vue
+++ WEET/resources/components/AppComponent.vue
@@ -2,6 +2,32 @@
     <div>
         <p class="text-8xl test-p">test</p>
     </div>
+    
+    <div class="demo-box">
+  <div class="flight-info">
+    <div class="segments">
+      <div class="segment departure">
+        <time>06:30</time>
+        <span class="airport">ALC</span>
+      </div>
+      <div class="divider"><span class="plane"></span></div>
+      <div class="segment adaptive">
+        <span class="airport">MAD</span>
+      </div>
+      <div class="divider adaptive">
+      <span class="plane"></span>
+      </div>
+      <div class="segment destination">
+        <time>11:40</time>
+        <span class="airport">VIE</span>
+      </div>
+    </div>
+  </div>
+  <div class="flight-duration">   
+  8h 40min Length of trip
+  </div>
+</div>
+<p class="info">Adaptive fluid flight info</p>
 </template>
 <script>
 export default {
