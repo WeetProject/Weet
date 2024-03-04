a6e781ae68bafb552168ad3f1d6b06de13fed4ef
jiwooo156 <wjdwldn156@naver.com>
2024-03-04 17:37:48 +0900

    0304 지우 마이페이지

---
+++ WEET/resources/components/MypageComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        마이페이지
+    </div>
+</template>
+<script>
+export default {
+    name: 'MypageComponent',
+}
+</script>
+<style lang="scss">
+	@import '../sass/mypage.scss';
+</style>
--- WEET/routes/web.php
+++ WEET/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return view('welcome');
 });
+
+Route::get('/mypage', function () {
+    return view('welcome');
+});
