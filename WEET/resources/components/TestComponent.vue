f81b53543f2b2c8e27fa79d7428dee780976a439
choibocknami12 <h_hcho1215@naver.com>
2024-03-04 23:25:15 +0900

    0304현_회원가입레이아웃중

--- WEET/resources/components/TestComponent.vue
+++ WEET/resources/components/TestComponent.vue
@@ -8,7 +8,10 @@
     <fieldset v-for="(fieldset, index) in fieldsets" :key="index" v-show="index === currentStep">
         <h2 class="fs-title">{{ fieldset.title }}</h2>
         <h3 class="fs-subtitle">{{ fieldset.subtitle }}</h3>
-            <input v-for="(input, inputIndex) in fieldset.inputs" :key="inputIndex" type="text" :name="input.name" :placeholder="input.placeholder" />
+            <!-- <input v-for="(input, inputIndex) in fieldset.inputs" :key="inputIndex" type="text" :name="input.name" :placeholder="input.placeholder" /> -->
+            <input v-for="(input, inputIndex) in fieldset.inputs" :key="inputIndex" 
+                :type="input.type" :name="input.name" :placeholder="input.placeholder" 
+                :class="input.classes" />
         <button v-if="index > 0" @click="prevStep" class="previous action-button">Previous</button>
         <button v-if="index < fieldsets.length - 1" @click="nextStep" class="next action-button">Next</button>
             <a v-if="index === fieldsets.length - 1" :href="submitLink" class="submit action-button" target="_top">Submit</a>
@@ -23,36 +26,27 @@ export default {
 
         data() {
             return {
-            currentStep: 0,
-            steps: ['Account Setup', 'Social Profiles', 'Personal Details'],
-            fieldsets: [
-                {
-                title: 'Create your account',
-                subtitle: 'This is step 1',
-                inputs: [
-                    { name: 'email', placeholder: 'Email' },
-                    { name: 'pass', placeholder: 'Password' },
-                    { name: 'cpass', placeholder: 'Confirm Password' }
-                ]
+                currentStep: 0,
+                steps: ['이용 약관 동의', '계정정보 입력'],
+                fieldsets: [
+                    {
+                        title: 'Create your account',
+                        subtitle: 'Agree to Terms and Conditions',
+                        inputs: [
+                            { name: 'email', placeholder: 'Email' },
+                            { name: 'pass', placeholder: 'Password' },
+                            { name: 'cpass', placeholder: 'Confirm Password' }
+                        ]
                 },
                 {
-                title: 'Social Profiles',
-                subtitle: 'Your presence on the social network',
-                inputs: [
-                    { name: 'twitter', placeholder: 'Twitter' },
-                    { name: 'facebook', placeholder: 'Facebook' },
-                    { name: 'gplus', placeholder: 'Google Plus' }
-                ]
-                },
-                {
-                title: 'Personal Details',
-                subtitle: 'We will never sell it',
-                inputs: [
-                    { name: 'fname', placeholder: 'First Name' },
-                    { name: 'lname', placeholder: 'Last Name' },
-                    { name: 'phone', placeholder: 'Phone' },
-                    { name: 'address', placeholder: 'Address' }
-                ]
+                    title: 'Create your account',
+                    subtitle: 'Personal Details',
+                    inputs: [
+                        { name: 'fname', placeholder: 'First Name' },
+                        { name: 'lname', placeholder: 'Last Name' },
+                        { name: 'phone', placeholder: 'Phone' },
+                        { name: 'address', placeholder: 'Address' }
+                    ]
                 }
             ],
             submitLink: 'https://twitter.com/GoktepeAtakan'
