4c9e5088352578e07972d11d2e0c8d95b735489b
YeoJunggi <muhwa_91@naver.com>
2024-03-10 08:42:27 +0900

    0310 admin 생성

---
+++ WEET/resources/components/AdminComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="admin_container">
+		<div class="admin_header_container">
+			<div class="admin_header_logo_section">
+				
+			</div>
+			<div class="admin_header_search_section">
+				
+			</div>
+			<div class="admin_header_information_section">
+				
+			</div>
+		</div>
+
+		<div class="admin_left_nav_container">
+			<a href="#">User</a>
+			<a href="#">Analytics</a>
+			<a href="#">Schedule</a>
+			<a href="#">Charts</a>
+			<a href="#">Logout</a>
+		</div>
+
+	</div>
+</template>
+<script>
+export default {
+    name:'AdminComponent',
+    
+}
+</script>
+<style lang="scss">
+    @import '../sass/Admin/admin.scss';
+</style>
