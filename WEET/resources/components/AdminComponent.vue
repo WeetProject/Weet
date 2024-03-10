46ff350076768c6e3d242326debb3cc4898d8f50
YeoJunggi <muhwa_91@naver.com>
2024-03-10 21:56:00 +0900

    0310 admin 메인페이지 틀 생성

--- WEET/resources/components/AdminComponent.vue
+++ WEET/resources/components/AdminComponent.vue
@@ -1,23 +1,54 @@
 <template>
     <div class="admin_container">
-		<div class="admin_header_container">
-			<div class="admin_header_logo_section">
+		<div class="admin_header_layout_container">
+			<div class="admin_header_left_logo_section">
 				
 			</div>
-			<div class="admin_header_search_section">
+			<div class="admin_header_middle_search_section">
 				
 			</div>
-			<div class="admin_header_information_section">
+			<div class="admin_header_right_information_section">
 				
 			</div>
 		</div>
 
-		<div class="admin_left_nav_container">
-			<a href="#">User</a>
-			<a href="#">Analytics</a>
-			<a href="#">Schedule</a>
-			<a href="#">Charts</a>
-			<a href="#">Logout</a>
+		<div class="admin_left_layout_container">
+			<div class="admin_left_nav_alink_section">
+				<a class="admin_left_nav_alink" href="#">User</a>
+				<a class="admin_left_nav_alink" href="#">Analytics</a>
+				<a class="admin_left_nav_alink" href="#">Schedule</a>
+				<a class="admin_left_nav_alink" href="#">Charts</a>
+				<a class="admin_left_nav_alink" href="#">Logout</a>
+			</div>
+		</div>
+		
+		<div class="admin_right_layout_container">
+			<div class="admin_right_top_total_section">
+				<div class="admin_right_total_area admin_right_total_order">
+
+				</div>
+				<div class="admin_right_total_area admin_right_total_revenue">
+
+				</div>
+				<div class="admin_right_total_area admin_right_total_user">
+
+				</div>
+				<div class="admin_right_total_area admin_right_total_post_area">
+
+				</div>
+			</div>
+
+			<div class="admin_right_left_analytics_section">
+				
+			</div>
+			<div class="admin_right_right_text_container">
+				<div class="admin_right_top_post_section">
+
+				</div>
+				<div class="admin_right_bottom_notice_section">
+
+				</div>
+			</div>
 		</div>
 
 	</div>
